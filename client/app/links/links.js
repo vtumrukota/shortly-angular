9481c08b19b86fe3d22105d2f97638d133fd01e9
vtumrukota <vtumrukota@gmail.com>
2015-04-22 21:53:24 -0700

    Make pretty the links

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,7 +1,6 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
   $scope.data = {
     links: []
   };
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,10 +1,8 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
   $scope.link = {};
   $scope.addLink = function(url){
-    console.log('url :', url);
     Links.addLink(url);
   };
 });
